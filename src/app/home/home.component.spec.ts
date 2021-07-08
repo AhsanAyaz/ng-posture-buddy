a35cad05a9e637bb012f786b8f4e24c3dffa7bbf
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-07-08 21:25:26 +0200

    fix(unit-tests): fixed unit tests by mocking ml5.min.js

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from "./home.component";
 import { TranslateModule } from "@ngx-translate/core";
 import { RouterTestingModule } from "@angular/router/testing";
 import { SharedModule } from "../shared/shared.module";
-
+jest.mock('./../../assets/ml5.min.js', ()=>(jest.fn()));
 describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
